9a6a3078dbb0d81ac3c83fa70fb07b2b0fe5b4be
Oscar García Peinado <oscar.garcia@bluekiri.com>
2018-01-29 11:49:30 +0100

    Fixed mongo database uri concatenation

--- dashboard_server/application/repositories/mongo_repository.py
+++ dashboard_server/application/repositories/mongo_repository.py
@@ -12,11 +12,13 @@ def get_mongo_connection():
 
 def get_concat_mongo_uri(database, mongo_uri):
     import re
-    match = re.match(r'(mongodb:\/\/[a-z:\._\-0-9]+)(\/.*)*', mongo_uri)
+    match = re.match(r'(mongodb:\/\/[a-z:\._\-0-9]+\/?)(.*)*', mongo_uri)
     slash_database = '/' + database
     if match is None or len(match.groups()) > 2:
         raise ConnectionError("Invalid mongo uri connection %s" % mongo_uri)
     match_groups = [item for item in match.groups() if item is not None]
     if len(match_groups) == 2:
-        return slash_database.join(match_groups)
+        return database.join(match_groups)
+    if mongo_uri[-1] == '/':
+        return mongo_uri + database
     return mongo_uri + slash_database
