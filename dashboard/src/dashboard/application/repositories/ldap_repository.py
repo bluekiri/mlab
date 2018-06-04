d58373a945f42b20e674e2647438caa5eb90e5f2
Oscar García Peinado <oscar.garcia@bluekiri.com>
2018-06-04 15:31:47 +0200

    Change config data sense variables

--- dashboard/src/dashboard/application/repositories/ldap_repository.py
+++ dashboard/src/dashboard/application/repositories/ldap_repository.py
@@ -6,10 +6,10 @@ from dashboard.application.conf.config import *
 def get_user_by_username_info(username):
     try:
         server = Server(LDAP_SERVER_URI)
-        conn = Connection(server, ldap_dn, ldap_pwd, auto_bind=True, version=3, authentication=SIMPLE,
+        conn = Connection(server, LDAP_DN, LDAP_PWD, auto_bind=True, version=3, authentication=SIMPLE,
                           receive_timeout=1)
         filter_account_name = '(|(%s=%s)(%s=%s))' % ("sAMAccountName", username, "mail", username)
-        conn.search(ldap_base, filter_account_name, attributes=ALL_ATTRIBUTES)
+        conn.search(LDAP_BASE, filter_account_name, attributes=ALL_ATTRIBUTES)
         return None if not len(conn.entries) else conn.entries[0]
     except:
         return None
